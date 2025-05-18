404bf15da9288bcf7bb7bb14099228f67c744e54
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-18 15:17:32 +0200

    last try

--- Assigments/02/code/start_SIMDreduce.sh
+++ Assigments/02/code/start_SIMDreduce.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-#SBATCH -p rome 
-#SBATCH -w asc-rome02     # oder z. B. asc-rome03, falls Gruppe-ID % 4 = 3
+#SBATCH -p rome
+#SBATCH -w asc-rome02
 #SBATCH --exclusive
 #SBATCH -o simd_reduce_output.txt
 
-export OMP_PLACES=numa_domains
-export OMP_PROC_BIND=true
+# 1. Spack aktivieren (wichtig, sonst kennt die Shell 'spack' nicht!)
+source /opt/asc/spack/share/spack/setup-env.sh
 
+# 2. Umgebung wie interaktiv vollständig laden
+source load_env_CPUAD.sh
+
+# 3. Debug-Output zur Kontrolle
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+ldd ./build/benchmarkSIMDreduce
+
+# 4. Benchmark starten
 ./build/benchmarkSIMDreduce
