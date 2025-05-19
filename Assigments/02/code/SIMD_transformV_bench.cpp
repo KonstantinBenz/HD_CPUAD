fb5c534c6668ee48d029ad767286018433277731
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-19 21:34:15 +0200

    fixed all segmentation faults

--- Assigments/02/code/SIMD_transformV_bench.cpp
+++ Assigments/02/code/SIMD_transformV_bench.cpp
@@ -13,14 +13,14 @@ int main(){
     SIMD_transform_view<ClockRecorder> test(rec);
 	
 	std::cout << "name,size,avg throughput,avg time,stdev time,cv\n";
-	// benchmarker(&test, &transform::benchTransformIterator, "benchTransformIterator", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchTransformIteratorInnerLoop, "benchTransformIteratorInnerLoop", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchTransformRange, "benchTransformRange", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchTransformRangeInnerLoop, "benchTransformRangeInnerLoop", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchTransformStl, "benchTransformStl", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchTransformSimdStl, "benchTransformSimdStl", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchXsimdTransform, "benchXsimdTransform", Nl, Nh, dur, timings,rec);
-	// benchmarker(&test, &transform::benchXsimdTransformAligned, "benchXsimdTransformAligned", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchTransformIterator, "benchTransformIterator", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchTransformIteratorInnerLoop, "benchTransformIteratorInnerLoop", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchTransformRange, "benchTransformRange", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchTransformRangeInnerLoop, "benchTransformRangeInnerLoop", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchTransformStl, "benchTransformStl", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchTransformSimdStl, "benchTransformSimdStl", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchXsimdTransform, "benchXsimdTransform", Nl, Nh, dur, timings,rec);
+	benchmarker(&test, &transform::benchXsimdTransformAligned, "benchXsimdTransformAligned", Nl, Nh, dur, timings,rec);
 	benchmarker(&test, &transform::benchOmpSimdTransformIterator, "benchOmpSimdTransformIterator", Nl, Nh, dur, timings,rec);
 	benchmarker(&test, &transform::benchOmpSimdTransformIteratorInnerLoop, "benchOmpSimdTransformIteratorInnerLoop", Nl, Nh, dur, timings,rec);
 	benchmarker(&test, &transform::benchOmpSimdTransformRange, "benchOmpSimdTransformRange", Nl, Nh, dur, timings,rec);
