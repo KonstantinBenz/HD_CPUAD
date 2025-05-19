d9943a690abca87d8807190b2cd44e8a9fc92700
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-19 19:58:44 +0200

    add start scripts

---
+++ Assigments/02/code/start_SIMDreduceV.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH -p rome
+#SBATCH -w asc-rome02   # oder 00/01/03 je nach groupID % 4
+#SBATCH --exclusive
+#SBATCH --export=ALL
+#SBATCH -o simd_reduce_output.txt
+
+export OMP_PLACES=numa_domains
+export OMP_PROC_BIND=true
+
+./build/benchmarkSIMDreduceV
---
+++ Assigments/02/code/start_SIMDtransformV.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH -p rome
+#SBATCH -w asc-rome02
+#SBATCH --exclusive
+#SBATCH --export=ALL
+#SBATCH -o simd_transform_output.txt
+
+export OMP_PLACES=numa_domains
+export OMP_PROC_BIND=true
+
+./build/benchmarkSIMDtransformV
