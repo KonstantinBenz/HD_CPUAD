3d7c8238578a4182ebc44710af3430c9db7e79d3
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-19 18:39:06 +0200

    again

--- Assigments/02/code/start_SIMDreduce.sh
+++ Assigments/02/code/start_SIMDreduce.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH -p rome
-#SBATCH -w asc-rome02
+#SBATCH -w asc-rome02   # oder 00/01/03 je nach groupID % 4
 #SBATCH --exclusive
 #SBATCH --export=ALL
 #SBATCH -o simd_reduce_output.txt
 
-
-# OpenMP-Settings
 export OMP_PLACES=numa_domains
 export OMP_PROC_BIND=true
 
-# Debug-Check
-ldd ./build/benchmarkSIMDreduce > debug_env.txt
-
-# Benchmark ausführen
-./build/benchmarkSIMDreduce >> simd_reduce_output.txt 2>&1
+./build/benchmarkSIMDreduce
--- Assigments/02/code/start_SIMDtransform.sh
+++ Assigments/02/code/start_SIMDtransform.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH -p rome 
+#SBATCH -p rome
 #SBATCH -w asc-rome02
 #SBATCH --exclusive
+#SBATCH --export=ALL
 #SBATCH -o simd_transform_output.txt
-
-#source load_env_CPUAD.sh
 
 export OMP_PLACES=numa_domains
 export OMP_PROC_BIND=true
