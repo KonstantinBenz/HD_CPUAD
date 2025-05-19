231f1e93668858b5519b9f1d2041f23f4d0f7882
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-19 20:11:59 +0200

    fix path

--- Assigments/02/code/start_SIMDtransformV.sh
+++ Assigments/02/code/start_SIMDtransformV.sh
@@ -3,7 +3,7 @@
 #SBATCH -w asc-rome02
 #SBATCH --exclusive
 #SBATCH --export=ALL
-#SBATCH -o simd_transform_output.txt
+#SBATCH -o simd_transformV_output.txt
 
 export OMP_PLACES=numa_domains
 export OMP_PROC_BIND=true
