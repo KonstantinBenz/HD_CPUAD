bad2f04a116166a9e1dbe4289353ad974b559736
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-20 13:51:47 +0200

    update

--- Assigments/03/code/ex03BenchmarkScript.sh
+++ Assigments/03/code/ex03BenchmarkScript.sh
@@ -9,9 +9,9 @@ export OMP_PROC_BIND=true
 
 cd /media/oldhome/kbenz/git/HD_CPUAD/Assigments/03/code/build
 
-echo "Starting REDUCE benchmark..."
-ls -lh ./reduceVbenchmarkUnroll
-./reduceVbenchmarkUnroll > reduce_results.csv 2>&1
+#echo "Starting REDUCE benchmark..."
+#ls -lh ./reduceVbenchmarkUnroll
+#./reduceVbenchmarkUnroll > reduce_results.csv 2>&1
 
 echo "Starting TRANSFORM benchmark..."
 ls -lh ./transformVbenchmarkUnroll
