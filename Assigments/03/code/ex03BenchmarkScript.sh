28cf1e816f0d95f2785dad948fa53af5725aa955
KonstantinBenz <konstantin.benz@protonmail.com>
2025-05-20 13:13:02 +0200

    fix shell script

--- Assigments/03/code/ex03BenchmarkScript.sh
+++ Assigments/03/code/ex03BenchmarkScript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -p rome
-#SBATCH -w asc-rome00
+#SBATCH -w asc-rome02
 #SBATCH --exclusive
 #SBATCH -o ex03_benchmark.out
 
@@ -9,10 +9,12 @@ export OMP_PROC_BIND=true
 
 cd /media/oldhome/kbenz/git/HD_CPUAD/Assigments/03/code/build
 
-#echo "Starting REDUCE benchmark..."
-#./reduceVbenchmarkUnroll > reduce_results.csv
+echo "Starting REDUCE benchmark..."
+ls -lh ./reduceVbenchmarkUnroll
+./reduceVbenchmarkUnroll > reduce_results.csv 2>&1
 
 echo "Starting TRANSFORM benchmark..."
-./transformVbenchmarkUnroll > transform_results.csv
+ls -lh ./transformVbenchmarkUnroll
+./transformVbenchmarkUnroll > transform_results.csv 2>&1
 
 echo "All benchmarks completed."
